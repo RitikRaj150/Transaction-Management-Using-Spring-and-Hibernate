dd11af806f3af56b1f2ce969598f5c5c3066404f
Ritik_Raj <ritikraj91428@gmail.com>
2025-11-03 10:55:55 +0530

    Create AccountDAO.java

---
+++ AccountDAO.java
@@ -0,0 +1,21 @@
+package com.nimbus.bank;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AccountDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public Account getAccount(int accNo) {
+        return sessionFactory.getCurrentSession().get(Account.class, accNo);
+    }
+
+    public void updateAccount(Account account) {
+        sessionFactory.getCurrentSession().update(account);
+    }
+}
